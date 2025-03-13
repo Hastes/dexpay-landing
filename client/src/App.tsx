8c7c3d24f244027afc6d19134703acb99a79b5f9
hastes <rom541997@gmail.com>
2025-03-13 10:06:50 +0700

    hash-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
-import { Switch, Route } from "wouter";
+import { Router as Wouter, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import WidgetDemo from "@/pages/WidgetDemo";
+import { useHashLocation } from "wouter/use-hash-location";
 
 function Router() {
   return (
-    <Switch>
+    <Wouter hook={useHashLocation}>
       <Route path="/" component={Home} />
       <Route path="/cryproswap-widget-demo" component={WidgetDemo} />
       <Route component={NotFound} />
-    </Switch>
+    </Wouter>
   );
 }
 
--- client/src/pages/WidgetDemo.tsx
+++ client/src/pages/WidgetDemo.tsx
@@ -2,8 +2,8 @@ export default function WidgetDemo() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <main className="flex-grow flex flex-col items-center justify-center py-16">
-        <p className="text-5xl font-semibold mb-7 tracking-wider">
-          Widget
+        <p className="text-4xl font-semibold mb-7 tracking-wider">
+          Iframe Widget
         </p>
         <iframe
           src="https://p2p.dextrade.com/swap-view?fromNetworkName=solana&fromTicker=SOL&toNetworkName=solana&toTicker=SOAR&name=MEW&widget=1"
