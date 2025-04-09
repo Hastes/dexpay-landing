8ad25c3a7b3ba76c7c84cfac5c9d6f1a31b24da6
hastes <rom541997@gmail.com>
2025-04-09 17:48:20 +0700

    wdiget: v2

--- client/src/pages/WidgetDemo.tsx
+++ client/src/pages/WidgetDemo.tsx
@@ -4,7 +4,7 @@ export default function WidgetDemo() {
       <main className="flex-grow flex flex-col items-center justify-center py-16">
       <div className="h-[500px] w-full">
           <iframe
-            src="https://p2p.dextrade.com/swap-widget?name=Soarchain&mode=dark&toNetworkName=solana&toTicker=SOAR"
+            src="https://p2p.dextrade.com/swap-widget?name=Soarchain&mode=light&toNetworkName=solana&toTicker=SOAR"
             width="100%"
             height="100%"
             title="DexPay Swap"
