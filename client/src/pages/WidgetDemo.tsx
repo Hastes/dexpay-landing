8fe35de6a67dd57077a7bc5e095338aaa8fe0e27
hastes <rom541997@gmail.com>
2025-04-09 17:47:15 +0700

    widget

--- client/src/pages/WidgetDemo.tsx
+++ client/src/pages/WidgetDemo.tsx
@@ -2,12 +2,15 @@ export default function WidgetDemo() {
   return (
     <div className="min-h-screen flex flex-col bg-[#F5F5FB]">
       <main className="flex-grow flex flex-col items-center justify-center py-16">
-        <iframe
-          src="http://localhost:5173/swap-widget?name=Pixel&mode=light"
-          width="100%"
-          height="600px"
-          title="DexPay Swap"
-        />
+      <div className="h-[500px] w-full">
+          <iframe
+            src="https://p2p.dextrade.com/swap-widget?name=Soarchain&mode=dark&toNetworkName=solana&toTicker=SOAR"
+            width="100%"
+            height="100%"
+            title="DexPay Swap"
+            style="border: none;"
+          />
+        </div>
       </main>
     </div>
   );
